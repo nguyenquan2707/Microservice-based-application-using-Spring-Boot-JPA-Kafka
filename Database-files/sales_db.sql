d2b667cc3c354d475d92d65dedff7fcdf8bdb11c
abhidas <abhi.das@gozo.cab>
2021-02-01 16:17:19 +0530

    - sales db structure updated

--- Database-files/sales_db.sql
+++ Database-files/sales_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 14, 2020 at 08:10 AM
+-- Generation Time: Feb 01, 2021 at 11:46 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -32,7 +32,7 @@ CREATE TABLE `card_payment` (
   `order_id` int(5) NOT NULL,
   `issuing_bank` varchar(50) NOT NULL,
   `card_type` varchar(30) NOT NULL,
-  `card_expiry_date` date NOT NULL,
+  `card_expiry_date` varchar(12) NOT NULL,
   `amount` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
@@ -48,9 +48,16 @@ CREATE TABLE `cheque_payment` (
   `bank_name` varchar(30) NOT NULL,
   `bank_branch` varchar(100) CHARACTER SET utf8 NOT NULL,
   `cheque_no` int(20) NOT NULL,
-  `cheque_date` date NOT NULL,
+  `cheque_date` varchar(12) NOT NULL,
   `amount` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `cheque_payment`
+--
+
+INSERT INTO `cheque_payment` (`id`, `order_id`, `bank_name`, `bank_branch`, `cheque_no`, `cheque_date`, `amount`) VALUES
+(35, 35, 'UBI', 'bpc', 1234, '22-09-2020', 440);
 
 -- --------------------------------------------------------
 
@@ -65,6 +72,14 @@ CREATE TABLE `customer` (
   `address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `name`, `mobile_no`, `address`) VALUES
+(30, 'Harugopal Das', '9734328919', 'Nadia, WB'),
+(31, 'Pinaki Pal', '7384709073', 'Nadia, WB');
+
 -- --------------------------------------------------------
 
 --
@@ -75,6 +90,14 @@ CREATE TABLE `customer_vs_total_sale` (
   `customer_id` int(5) NOT NULL,
   `total_sale` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `customer_vs_total_sale`
+--
+
+INSERT INTO `customer_vs_total_sale` (`customer_id`, `total_sale`) VALUES
+(30, 1),
+(31, 2);
 
 -- --------------------------------------------------------
 
@@ -91,6 +114,15 @@ CREATE TABLE `item` (
   `tax_percentage` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`id`, `name`, `unit`, `stock_quantity`, `unit_price`, `tax_percentage`) VALUES
+(1, 'Amul Milk', 'ltr', 94, 21, 5),
+(7, 'Almond Milk', 'ltr', 10, 70, 3),
+(9, 'Butter Milk', 'ltr', 80, 70, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -100,10 +132,17 @@ CREATE TABLE `item` (
 CREATE TABLE `order` (
   `id` int(5) NOT NULL,
   `delivery_address` varchar(100) NOT NULL,
-  `order_date` date NOT NULL,
+  `order_date` varchar(100) NOT NULL,
   `total` int(5) NOT NULL,
   `customerId` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `delivery_address`, `order_date`, `total`, `customerId`) VALUES
+(35, 'MSD, WB', '21-01-2021', 5500, 31);
 
 -- --------------------------------------------------------
 
@@ -125,6 +164,18 @@ CREATE TABLE `orderLine` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
+-- Dumping data for table `orderLine`
+--
+
+INSERT INTO `orderLine` (`id`, `amount`, `itemId`, `line_total`, `orderId`, `quantity`, `tax_amount`, `tax_percentage`, `unit`, `unit_cost`) VALUES
+(6, 220, 1, 440, 12, 2, 3, 0, 'ltr', 20),
+(7, 220, 1, 440, 13, 2, 3, 0, 'ltr', 20),
+(8, 220, 1, 440, 14, 2, 3, 0, 'ltr', 20),
+(9, 220, 1, 440, 15, 2, 3, 0, 'ltr', 20),
+(10, 220, 1, 440, 16, 2, 3, 0, 'ltr', 20),
+(29, 220, 1, 440, 35, 2, 3, 0, 'ltr', 20);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -140,7 +191,7 @@ ALTER TABLE `card_payment`
 --
 ALTER TABLE `cheque_payment`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `cheque_payment_ibfk_1` (`order_id`);
+  ADD KEY `fk_ch_payment_orderid_order_id` (`order_id`);
 
 --
 -- Indexes for table `customer`
@@ -165,7 +216,7 @@ ALTER TABLE `item`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `customerId` (`customerId`);
+  ADD KEY `fk_order_cid_cust_id` (`customerId`);
 
 --
 -- Indexes for table `orderLine`
@@ -189,31 +240,31 @@ ALTER TABLE `card_payment`
 -- AUTO_INCREMENT for table `cheque_payment`
 --
 ALTER TABLE `cheque_payment`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `orderLine`
 --
 ALTER TABLE `orderLine`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Constraints for dumped tables
@@ -229,7 +280,8 @@ ALTER TABLE `card_payment`
 -- Constraints for table `cheque_payment`
 --
 ALTER TABLE `cheque_payment`
-  ADD CONSTRAINT `cheque_payment_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+  ADD CONSTRAINT `fk_ch_payment_orderid_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_cp_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `customer_vs_total_sale`
@@ -241,6 +293,7 @@ ALTER TABLE `customer_vs_total_sale`
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
+  ADD CONSTRAINT `fk_order_cid_cust_id` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customer_vs_total_sale` (`customer_id`) ON DELETE CASCADE;
 
