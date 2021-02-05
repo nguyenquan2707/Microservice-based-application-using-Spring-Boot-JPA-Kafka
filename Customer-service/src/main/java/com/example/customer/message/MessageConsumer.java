66bb1690097421acc39e6e00590d763022030b54
abhidas <abhi.das@gozo.cab>
2021-02-05 13:34:57 +0530

    - MessageConsumer.java updated to insert/add new customer into DB

--- Customer-service/src/main/java/com/example/customer/message/MessageConsumer.java
+++ Customer-service/src/main/java/com/example/customer/message/MessageConsumer.java
@@ -1,6 +1,8 @@
 package com.example.customer.message;
 
+import com.example.customer.model.dao.ICustomerDao;
 import com.example.customer.model.dao.ICustomerSaleDao;
+import com.example.customer.model.entity.Customer;
 import com.example.customer.model.entity.CustomerSale;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,19 +18,39 @@ public class MessageConsumer {
 
     private final String TOPIC_UPDATE_SALE_INFO ="updateSaleInfoCallback";
 
+    private final String TOPIC_REGISTER_NEW_CUSTOMER ="regNewCustomerCallback";
+
+
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
     ICustomerSaleDao iCustomerSaleDao;
 
+    @Autowired
+    ICustomerDao iCustomerDao;
+
     @KafkaListener(topics = TOPIC_UPDATE_SALE_INFO)
-    void regCustomerCallback(String message){
+    void updateSaleInfoCallback(String message){
 
         try {
             CustomerSale sale = objectMapper.readValue(message, CustomerSale.class);
 
             iCustomerSaleDao.updateSale(sale);
+
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage(),e);
+        }
+
+    }
+
+    @KafkaListener(topics = TOPIC_REGISTER_NEW_CUSTOMER)
+    void regNewCustomerCallback(String message){
+
+        try {
+            Customer customer = objectMapper.readValue(message, Customer.class);
+
+            iCustomerDao.addCustomer(customer);
 
         } catch (JsonProcessingException e) {
             logger.error(e.getMessage(),e);
