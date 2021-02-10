19290a9d8f810f5007a4430dd99f864d466e5f7b
abhi.das <abhi.das@gozo.cab>
2021-02-10 13:15:28 +0530

    customer_db.sql updated

--- Database-files/customer_db.sql
+++ Database-files/customer_db.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 30, 2020 at 09:03 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 10, 2021 at 07:44 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,11 +27,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
   `id` int(5) NOT NULL,
   `name` varchar(30) NOT NULL,
   `mobile_no` varchar(15) NOT NULL,
-  `address` varchar(100) NOT NULL
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
@@ -40,10 +41,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `name`, `mobile_no`, `address`) VALUES
-(29, 'haru das', '9734328919', 'berhamproe'),
-(30, 'Abhishek Das', '9609275425', 'Palashipara'),
-(31, 'Tanusha Das', '9474220262', 'Habra, Kolkata'),
-(32, 'partha sarathi das', '9830335768', 'berhampore');
+(46, 'Pinaki Pal', '8759161626', 'Berhampore, WB');
 
 -- --------------------------------------------------------
 
@@ -51,9 +49,11 @@ INSERT INTO `customer` (`id`, `name`, `mobile_no`, `address`) VALUES
 -- Table structure for table `customer_vs_total_sale`
 --
 
-CREATE TABLE `customer_vs_total_sale` (
+DROP TABLE IF EXISTS `customer_vs_total_sale`;
+CREATE TABLE IF NOT EXISTS `customer_vs_total_sale` (
   `customer_id` int(5) NOT NULL,
-  `total_sale` int(3) NOT NULL
+  `total_sale` int(3) NOT NULL,
+  PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
@@ -61,36 +61,7 @@ CREATE TABLE `customer_vs_total_sale` (
 --
 
 INSERT INTO `customer_vs_total_sale` (`customer_id`, `total_sale`) VALUES
-(29, 0),
-(30, 20),
-(31, 10),
-(32, 0);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `customer_vs_total_sale`
---
-ALTER TABLE `customer_vs_total_sale`
-  ADD PRIMARY KEY (`customer_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+(46, 1);
 
 --
 -- Constraints for dumped tables
@@ -100,8 +71,7 @@ ALTER TABLE `customer`
 -- Constraints for table `customer_vs_total_sale`
 --
 ALTER TABLE `customer_vs_total_sale`
-  ADD CONSTRAINT `customer_vs_total_sale_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `customer_vs_total_sale_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+  ADD CONSTRAINT `fk_cust_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
